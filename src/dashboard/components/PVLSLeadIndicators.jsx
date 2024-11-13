51c82ee0a11fcae1f48e63069a61f0ea6fea0a12
chineduprincewill <chinedu.ijeomah@yahoo.com>
2024-11-13 12:03:25 +0100

    ui fix

--- src/dashboard/components/PVLSLeadIndicators.jsx
+++ src/dashboard/components/PVLSLeadIndicators.jsx
@@ -4,20 +4,56 @@ const PVLSLeadIndicators = () => {
     return (
         <div className='w-full md:w-[32%] md:flex md:justify-between py-4'>
             <div className='w-full grid md:w-[23%] p-2 rounded-md shadow-xl border border-gray-300 dark:border-gray-700'>
-                <span className='text-lg'>192</span>
-                <span className='text-xs'>Eligibles</span>
+                <div className='flex justify-between items-baseline border-b border-gray-300 dark:border-gray-700 py-1'>
+                    <span className='text-3xl font-bold'>178,494</span>
+                    <span className='text-xs'>Eligibles</span>
+                </div>
+                <div className='py-2 flex justify-between items-baseline'>
+                    <span className='text-[10px]'>Eligibles covered</span>
+                    <span className='flex p-1 rounded-full shadow-md'>
+                        <span className='text-xl font-bold'>92</span>
+                        <span className='text-xs'>%</span>
+                    </span>
+                </div>
             </div>
             <div className='w-full grid md:w-[23%] p-2 rounded-md shadow-xl border border-gray-300 dark:border-gray-700'>
-                <span className='text-lg'>214</span>
-                <span className='text-xs'>PITC</span>
+                <div className='flex justify-between items-baseline border-b border-gray-300 dark:border-gray-700 py-1'>
+                    <span className='text-3xl font-bold'>...</span>
+                    <span className='text-xs'>...</span>
+                </div>
+                <div className='py-2 flex justify-between items-baseline'>
+                    <span className='text-[10px]'>Sample-Result Rate</span>
+                    <span className='flex p-1 rounded-full shadow-md'>
+                        <span className='text-xl font-bold'>95</span>
+                        <span className='text-xs'>%</span>
+                    </span>
+                </div>
             </div>
             <div className='w-full grid md:w-[23%] p-2 rounded-md shadow-xl border border-gray-300 dark:border-gray-700'>
-                <span className='text-lg'>55</span>
-                <span className='text-xs'>Key Pop</span>
+                <div className='flex justify-between items-baseline border-b border-gray-300 dark:border-gray-700 py-1'>
+                    <span className='text-3xl font-bold'>...</span>
+                    <span className='text-xs'>...</span>
+                </div>
+                <div className='py-2 flex justify-between items-baseline'>
+                    <span className='text-[10px]'>Rejection Rate</span>
+                    <span className='flex p-1 rounded-full shadow-md'>
+                        <span className='text-xl font-bold'>0</span>
+                        <span className='text-xs'>%</span>
+                    </span>
+                </div>
             </div>
             <div className='w-full grid md:w-[23%] p-2 rounded-md shadow-xl border border-gray-300 dark:border-gray-700'>
-                <span className='text-lg'>226</span>
-                <span className='text-xs'>Testing TAT</span>
+                <div className='flex justify-between items-baseline border-b border-gray-300 dark:border-gray-700 py-1'>
+                    <span className='text-6xl font-bold'>6.8</span>
+                    <span className='text-xs'>.</span>
+                </div>
+                <div className='py-2 flex justify-between items-baseline'>
+                    <span className='text-[10px]'>DAYS Testing TAT</span>
+                    <span className='flex p-1 rounded-full shadow-md'>
+                        <span className='text-xl font-bold'>0</span>
+                        <span className='text-xs'>%</span>
+                    </span>
+                </div>
             </div>
         </div>
     )
--- src/dashboard/components/TXCURRLeadIndicators.jsx
+++ src/dashboard/components/TXCURRLeadIndicators.jsx
@@ -4,20 +4,56 @@ const TXCURRLeadIndicators = () => {
   return (
     <div className='w-full md:w-[32%] md:flex md:justify-between py-4'>
         <div className='w-full grid md:w-[23%] p-2 rounded-md shadow-xl border border-gray-300 dark:border-gray-700'>
-            <span className='text-lg'>192</span>
-            <span className='text-xs'>PMTCT</span>
+            <div className='flex justify-between items-baseline border-b border-gray-300 dark:border-gray-700 py-1'>
+                <span className='text-3xl font-bold'>8</span>
+                <span className='text-xs'>PMTCT</span>
+            </div>
+            <div className='py-2 flex justify-between items-baseline'>
+                <span className='text-[10px]'>PMTCT Stream</span>
+                <span className='flex p-1 rounded-full shadow-md'>
+                    <span className='text-xl font-bold'>5</span>
+                    <span className='text-xs'>%</span>
+                </span>
+            </div>
         </div>
         <div className='w-full grid md:w-[23%] p-2 rounded-md shadow-xl border border-gray-300 dark:border-gray-700'>
-            <span className='text-lg'>214</span>
-            <span className='text-xs'>TX_NEW</span>
+            <div className='flex justify-between items-baseline border-b border-gray-300 dark:border-gray-700 py-1'>
+                <span className='text-3xl font-bold'>58</span>
+                <span className='text-xs'>TX_NEW</span>
+            </div>
+            <div className='py-2 flex justify-between items-baseline'>
+                <span className='text-[10px]'>Linkage rate</span>
+                <span className='flex p-1 rounded-full shadow-md'>
+                    <span className='text-xl font-bold'>100</span>
+                    <span className='text-xs'>%</span>
+                </span>
+            </div>
         </div>
         <div className='w-full grid md:w-[23%] p-2 rounded-md shadow-xl border border-gray-300 dark:border-gray-700'>
-            <span className='text-lg'>55</span>
-            <span className='text-xs'>IIT</span>
+            <div className='flex justify-between items-baseline border-b border-gray-300 dark:border-gray-700 py-1'>
+                <span className='text-3xl font-bold'>67,650</span>
+                <span className='text-xs'>IIT</span>
+            </div>
+            <div className='py-2 flex justify-between items-baseline'>
+                <span className='text-[10px]'>Retention rate</span>
+                <span className='flex p-1 rounded-full shadow-md'>
+                    <span className='text-xl font-bold'>73</span>
+                    <span className='text-xs'>%</span>
+                </span>
+            </div>
         </div>
         <div className='w-full grid md:w-[23%] p-2 rounded-md shadow-xl border border-gray-300 dark:border-gray-700'>
-            <span className='text-lg'>226</span>
-            <span className='text-xs'>Returned</span>
+            <div className='flex justify-between items-baseline border-b border-gray-300 dark:border-gray-700 py-1'>
+                <span className='text-3xl font-bold'>3,516</span>
+                <span className='text-xs'>Returned</span>
+            </div>
+            <div className='py-2 flex justify-between items-baseline'>
+                <span className='text-[10px]'>...</span>
+                <span className='flex p-1 rounded-full shadow-md'>
+                    <span className='text-xl font-bold'>0</span>
+                    <span className='text-xs'>%</span>
+                </span>
+            </div>
         </div>
     </div>
   )
